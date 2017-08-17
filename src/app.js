24f5f27b75cec31c93d77bf6d946d4aea6b924cf
ibrobabs <babskolawole@gmail.com>
2017-08-17 06:40:10 -0600

    update

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import firebase from 'firebase';
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 
 class App extends Component {
@@ -20,7 +21,7 @@ class App extends Component {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <Text>An App</Text>
+                <LoginForm/>
             </View>
         );
     }
